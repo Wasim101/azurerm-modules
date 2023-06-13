d840333b2943c524a149da50029b35e6f19eb724
wasim <64001734+Wasim101@users.noreply.github.com>
2023-06-13 07:38:05 +0530

    Update variables.tf

--- az-resource-group/variables.tf
+++ az-resource-group/variables.tf
@@ -1,7 +1,9 @@
 variable "rg-name" {
     type=string
+    description = "Used for a resource group name"
 }
 
 variable "rg-location" {
     type=string
+    description = "Used for a resource group location"
 }
